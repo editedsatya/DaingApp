7518589b79d5a7c3b48152b0a4ab3e19443cc106
Satya Gupta <Satya Gupta>
2019-04-25 23:58:13 +0530

    initial Commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext:DbContext
+    {
+       public DataContext(DbContextOptions<DataContext>option):base(option){}   
+       public DbSet<value> values { get; set; }
+    }
+}
